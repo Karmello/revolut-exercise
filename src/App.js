0755a647c192151e309dacec95363c4a17b442d7
Karmello <nogakamil@vp.pl>
2021-02-19 17:47:00 +0100

    added prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import { hot } from "react-hot-loader";
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
 
-import "./App.css";
+import './App.css';
 
 class App extends Component {
   render() {
